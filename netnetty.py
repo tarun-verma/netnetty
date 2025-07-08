b64dbcec9654b41e88d5074ea82bbf3e3fb03bd7
tarun-verma <tr.vrma@gmail.com>
2025-07-08 23:25:23 +0100

    scrub key

--- netnetty.py
+++ netnetty.py
@@ -8,7 +8,7 @@ from pprint import pprint
 import dns.resolver
 
 
-client = genai.Client(api_key="AIzaSyCF6ezrbk36o4gCArMCzbMM5xap7jpQA0c")
+client = genai.Client(api_key="")
 
 record_types = {
     'NONE', 'A', 'NS', 'MD', 'MF', 'CNAME', 'SOA', 'MB', 'MG', 'MR', 'NULL', 'WKS', 'PTR', 'HINFO', 'MINFO', 'MX', 'TXT', 'RP', 'AFSDB', 'X25', 'ISDN', 'RT', 'NSAP', 'NSAP-PTR', 'SIG', 'KEY', 'PX', 'GPOS', 'AAAA', 'LOC', 'NXT', 'SRV', 'NAPTR', 'KX', 'CERT', 'A6', 'DNAME', 'OPT', 'APL', 'DS', 'SSHFP', 'IPSECKEY', 'RRSIG', 'NSEC', 'DNSKEY', 'DHCID', 'NSEC3', 'NSEC3PARAM', 'TLSA', 'HIP', 'CDS', 'CDNSKEY', 'CSYNC', 'SPF', 'UNSPEC', 'EUI48', 'EUI64', 'TKEY', 'TSIG', 'IXFR', 'AXFR', 'MAILB', 'MAILA', 'ANY', 'URI', 'CAA', 'TA', 'DLV'
